7789f70c27c48ff83208dcd0c8689d436c2fed8b
Fig77 <55416179+Fig77@users.noreply.github.com>
2020-09-29 14:34:19 -0300

    sort of seems to work with postgres...

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,5 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+ENV['TRACKING_DATABASE_USER']="trackinguser"
+ENV['TRACKING_DATABASE_PASSWORD']="imhelping"
