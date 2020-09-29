037d3e7edf9a541aeb3cc7eec33a4e3e255932d2
Fig77 <55416179+Fig77@users.noreply.github.com>
2020-09-29 14:54:25 -0300

    fixed syntax errors

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,7 +5,7 @@ ENV['RAILS_ENV'] ||= 'test'
 # require File.expand_path('../config/environment', __dir__)
 
 # for the sake of adding spec as autoload, the above line is commented
-Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+Dir[Rails.root.join('spec/support/**/*.rb')].sort.each { |f| require f }
 
 # Prevent database truncation if the environment is production
 abort('The Rails environment is running in production mode!') if Rails.env.production?
