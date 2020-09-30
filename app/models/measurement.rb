d7a40e747ba5dd5ccd0a2f5eedbc8dc2f4423cdb
Fig77 <55416179+Fig77@users.noreply.github.com>
2020-09-30 16:06:25 -0300

    Measurement model has the correct validations now

--- app/models/measurement.rb
+++ app/models/measurement.rb
@@ -1,4 +1,7 @@
 class Measurement < ApplicationRecord
   belongs_to :user, class_name: 'User', foreign_key: 'users_id'
   belongs_to :measure_item, class_name: 'MeasureItem', foreign_key: 'measure_items_id'
+
+  validates_presence_of :value
+  validates_numericality_of :value, :greater_than => 0
 end
