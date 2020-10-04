90cc274135cae667ddcddbf745cc3f1a96448bc9
Fig77 <55416179+Fig77@users.noreply.github.com>
2020-10-04 13:37:23 -0300

    Adding auth

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,19 @@
 class UsersController < ApplicationController
-  def create; end
+  skip_before_action :authorize_request, only: :authenticate
+
+  def create
+  end
+
+  def update # only on himself
+  end
+
+  private
+
+  def user_params
+    params.permit(:username, :email, :password_digest)
+  end
+
+  def user_update
+    params.permit(:email)
+  end
 end
