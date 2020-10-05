3ccc2008abb1bcb6b5f73e8a6df8b6e437cd9974
Fig77 <55416179+Fig77@users.noreply.github.com>
2020-10-05 00:11:49 -0300

    Adding user authentication account controller, and create account action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,18 @@
 class UsersController < ApplicationController
   skip_before_action :authorize_request, only: :authenticate
 
-  def create; end
+  def create
+    @user = User.create!(user_params)
+    auth_token = AuthenticateUser.new(user.email, user.password).call
+    response = { message: Message.account_created, auth_token: auth_token }
+    json_response(response, :created)
+    json_response(@user, :created)
+  end
 
-  def update; end
+  def update
+    @user.update(user_update)
+    head :no_content
+  end
 
   private
 
