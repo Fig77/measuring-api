bc87e49cf74d5254b5b0a79dd30928a2607e6dc7
Fig77 <55416179+Fig77@users.noreply.github.com>
2020-10-11 22:52:06 -0300

    Added versioning for current implementation

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   resources :measure_items, only: [:index, :show]
   resources :measurements
-  post 'auth/login', to: 'authentication#authenticate'
-  post '/signup', to: 'users#create'
 
   namespace :admin do
     resources :users, only: [:index, :show, :update, :destroy]
     resources :measure_items, only: [:create, :update, :destroy]
   end
+
+  post 'auth/login', to: 'authentication#authenticate'
+  post '/signup', to: 'users#create'
 end
